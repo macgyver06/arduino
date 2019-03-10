ea9d787fcba87398dec57b1d35081a561eb216ac
macgyver06 <45843189+macgyver06@users.noreply.github.com>
2019-03-10 12:08:54 +0100

    Update and rename rapports seances/Controleur_Gestuel_version_initiale.ino to rapports seances/ORAL/programme_Arduino_2.ino

--- rapports seances/ORAL/programme_Arduino_2.ino
+++ rapports seances/ORAL/programme_Arduino_2.ino
@@ -23,7 +23,7 @@ const int echo1 = 3;
 const int trigger2 = 4; 
 const int echo2 = 5; 
 
-double dist1,dist2,distL,distR,diff; 
+double distL,distR; 
 
 NewPing sonar1(trigger1, echo1,200); // le module HC-SR04 de gauche
 NewPing sonar2(trigger2, echo2,200); // et celui de droite
@@ -44,7 +44,7 @@ void loop() {
 // PLAY/PAUSE
 if ((distL >1 && distR>1) && (distL <10 && distR<10)) {   // dans cette version, pour ne pas mal interpreter un geste, on met tout simplement une plage de distance differente pour le geste PLAY/PAUSE
   Serial.println("PLAY/PAUSE"); 
-  delay (1000);
+  delay (1000); // on laisse le temps à l'utilisateur de retirer ses mains
   }
   
 
@@ -53,17 +53,17 @@ if ((distL >1 && distR>1) && (distL <10 && distR<10)) {   // dans cette version,
  
 // SON--/SON++
 if (distL>=15 && distL<=35){
-  delay(100); 
+  delay(500); 
   distL = sonar1.ping_cm();
   if (distL>=15 && distL<=35){
-    Serial.println("main gauche OK"); // permet de voir sur la sortie du programme python quand ce geste est reperé
+    Serial.println("main gauche OK"); // permet de verifier que le module fonctionne bien
     while(distL<=35){
       distL = sonar1.ping_cm();
       if (distL<20){
-        Serial.println ("SON--"); delay (300);    
+        Serial.println ("SON--");   
         }
       if (distL>20){
-        Serial.println ("SON++"); delay (300);
+        Serial.println ("SON++"); 
         }
     }
   }
@@ -71,20 +71,20 @@ if (distL>=15 && distL<=35){
 
 // REWIND/FORWARD
 if (distR>=15 && distR<=35){
-  delay(100); 
+  delay(500); 
   distR = sonar2.ping_cm();
   if (distR>=15 && distR<=35){
     Serial.println("main droite OK"); 
     while(distR<=35){
       distR = sonar2.ping_cm();
       if (distR<20) {
-        Serial.println ("Rewind"); delay (300);
+        Serial.println ("Rewind"); 
         }
       if (distR>20){
-        Serial.println ("Forward"); delay (300);
+        Serial.println ("Forward"); 
         }
     }
   }
 }
-delay(200);
+
 }
